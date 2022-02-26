c2d9c96a89bfb253c7ed980805f453da0383a1dd
Luis Montealegre <montealegreluis@gmail.com>
2022-02-26 14:59:54 -0600

    refactor!: Make ProblemTypeConverter final
    BREAKING CHANGE: `ProblemTypeConverter` is now a final class

--- src/main/java/com/montealegreluis/apiproblemspringboot/jackson/ProblemTypeConverter.java
+++ src/main/java/com/montealegreluis/apiproblemspringboot/jackson/ProblemTypeConverter.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.util.StdConverter;
 import com.montealegreluis.apiproblem.ApiProblem;
 import java.net.URI;
 
-public class ProblemTypeConverter extends StdConverter<URI, URI> {
+public final class ProblemTypeConverter extends StdConverter<URI, URI> {
   @Override
   public URI convert(URI value) {
     return ApiProblem.DEFAULT_TYPE.equals(value) ? null : value;
