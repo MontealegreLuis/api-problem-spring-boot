ba45d5a5482a8030558d15b35714ac1a99410e61
Luis Montealegre <montealegreluis@gmail.com>
2022-02-26 14:58:39 -0600

    fix: Prevent auto-detecting properties from mixin
    - This will prevent including `additionalProperties` in JSON value
    - Make the mix-in public so it can also be used directly

--- src/main/java/com/montealegreluis/apiproblemspringboot/jackson/ApiProblemMixIn.java
+++ src/main/java/com/montealegreluis/apiproblemspringboot/jackson/ApiProblemMixIn.java
@@ -2,16 +2,15 @@ package com.montealegreluis.apiproblemspringboot.jackson;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.montealegreluis.apiproblem.Problem;
 import java.net.URI;
 import java.util.Map;
 
 @JsonInclude(NON_EMPTY)
-interface ApiProblemMixIn extends Problem {
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE)
+public interface ApiProblemMixIn extends Problem {
   @JsonProperty("type")
   @JsonSerialize(converter = ProblemTypeConverter.class)
   @Override
